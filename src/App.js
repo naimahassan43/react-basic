f00f8a8b593a2b1890c077acc62aee286ff1aed6
Naima Hassan <naima.hassan434@gmail.com>
2021-08-26 20:43:36 +0100

    JSX rules

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 /****** Class Based Component ********/
+// import React from "react";
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <p> Hello!!!!!!!!</p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+/****** Function Based Component ********/
+
+// import React from "react";
+// const App = () => {
+//   return (
+//     <div>
+//       <h1> Hello From Functional Component!!!!!!!!</h1>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+/****** JSX Rules ********/
+
 import React from "react";
 
-class App extends React.Component {
-  render() {
-    return (
+const App = () => {
+  const country = "Bangladesh";
+  const num1 = 2999;
+  const num2 = -99;
+  return (
+    <React.Fragment>
       <div>
-        <p> Hello!!!!!!!!</p>
+        <h1> Hello From {country}!!!!</h1>
+        <h2> {num1 - num2}</h2>
+        <h3>{num2 > 0 ? "Positive" : "Negative"}</h3>
       </div>
-    );
-  }
-}
+      <div>
+        <h1> Hello From {country}!!!!</h1>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default App;
