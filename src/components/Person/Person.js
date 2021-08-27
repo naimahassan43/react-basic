e7089335e8dfb318bca3309a1aab1d5c8c17bea4
Naima Hassan <naima.hassan434@gmail.com>
2021-08-27 01:03:47 +0100

    CSS in JSX

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,17 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./style.css";
+// import "./style.css";
 
 const Person = (props) => {
   const { name, country } = props;
   return (
-    <div className="container">
+    <div className="container" style={styleSheet}>
       <h2> Hello Everyone! I am {name}! </h2>
       <h3> I am from {country} </h3>
     </div>
   );
 };
 
+const styleSheet = {
+  padding: "50px",
+  color: "#afeccf",
+  background: " rgb(42, 2, 2)",
+  fontSize: "20px",
+};
 Person.propTypes = {
   name: PropTypes.string.isRequired,
   country: PropTypes.string.isRequired,
